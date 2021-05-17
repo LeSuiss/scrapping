a67f7069fb58fea2369ee7752f35357a4ec69a1a
lesuiss <alexis.archer44@gmail.com>
2021-05-17 15:52:56 +0200

    ok recup, en attente quentin pour instruction

--- puppeteer.js
+++ puppeteer.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const tss  = require('./helpers/puppeteer').takeScreenshot;
-
+const util = require('util');
 (async () => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
@@ -9,7 +9,7 @@ const tss  = require('./helpers/puppeteer').takeScreenshot;
     const buttons = await page.$$('.jss93')
     await buttons[1].click(),
     
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(3000)
     await tss(page);
     const frameHandle = await page.$("#wallet");
     const frame = await frameHandle.contentFrame();
@@ -19,12 +19,47 @@ const tss  = require('./helpers/puppeteer').takeScreenshot;
     await frame.type('input[type="password"]', 'bijour11'),
     await frame.click('button[type="submit"]')
     await page.waitForNavigation()
-    await page.goto('https://sorare.com/market/new_signings?refinementList[sale.bundled][0]=false');
-    console.log("test")
-    await page.waitForTimeout(1000)
+    page.on('response', async (response) => {
+        try{
+            const result = await response.json()    
+            !!result && !! result.data  && result.data.card && console.log("************************************************************************************************",util.inspect(await response.json(), true, null, true)); 
+        }
+        catch(err){}
+        
+    }); 
 
-    const stories = await page.$$eval('a', as => as.map(a => a.href));
-    console.log(stories.filter(x=>x.includes('https://sorare.com/cards/')))
-    //   console.log(response);
+    let AllCardsUrl = []
+    let hasFoundPageWithCards = true;
+    let i = 1
+    const getCardsUrl = async(runningPage)=>{
+        try{
+            const AllLinks = await runningPage.$$eval('a', as => as.map(a => a.href));
+            const cardLinks = AllLinks.filter(x=>x.includes('https://sorare.com/cards/'));
+            if (cardLinks.length === 0) {
+                hasFoundPageWithCards= false
+            }
+            else{
+                cardLinks.map(x=>AllCardsUrl.push(x))
+
+            }
+        }
+        catch(err){
+            console.log(err)
+            hasFoundPageWithCards = false
+        }
+    }
+    while (hasFoundPageWithCards && i <2) {
+        await page.goto(`https://sorare.com/market/new_signings?refinementList%5Bsale.bundled%5D%5B0%5D=false&page=${i}&configure%5BhitsPerPage%5D=20&configure%5Bfilters%5D=sale.primary%3Atrue%20AND%20visible%3D1%20AND%20%28rarity%3Arare%20OR%20rarity%3Asuper_rare%20OR%20rarity%3Aunique%29&configure%5Bdistinct%5D=true&configure%5BattributesToRetrieve%5D%5B0%5D=sale`);
+        i++;
+        await page.waitForTimeout(1000);
+        await getCardsUrl(page)
+    }
+ 
+    
+    
+
+console.log('AllCardsUrl |||  ' ,typeof(AllCardsUrl),'  |||  ', AllCardsUrl)
+await page.goto(AllCardsUrl[0]) 
+//   console.log(response);
     // await browser.close();
 })();
